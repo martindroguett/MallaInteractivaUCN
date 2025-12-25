169d991a6ee5d440c27332c59fac170d69b84130
martindroguett <martin.droguett@alumnos.ucn.cl>
2025-12-25 00:01:03 -0300

    Creación de clase asignatura y creación de documento JSON de malla ICCI, ordenado por semestre y asignaturas en orden alfabetico.

---
+++ js/Asignatura.js
@@ -0,0 +1,8 @@
+export class Asignatura{
+    constructor(id, nombre, creditos, prerrequisitos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.prerrequisitos = prerrequisitos;
+    }
+}
